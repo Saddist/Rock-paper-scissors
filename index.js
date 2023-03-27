f8a9ae52bcc77e11dcff4a3e4cbce5474da08468
Saddist <74913287+Saddist@users.noreply.github.com>
2023-03-28 01:43:23 +0530

    Added game function

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ function getComputerChoice() {
     const choice = options[Math.floor(Math.random() * options.length)];
     return choice;
 }
-let computerSelection = getComputerChoice();
-let playerSelection = prompt("Choose rock papers or scissors","rock");
 function compare(playerSelection, computerSelection){
     if(playerSelection == computerSelection){
         return "Tie";
@@ -17,14 +15,48 @@ function compare(playerSelection, computerSelection){
         return "Computer";
     }
 }
-switch (compare(playerSelection, computerSelection)) {
-    case "Tie":
-        console.log("NO one wins");
-        break;
-    case "Player":
-        console.log("Player wins");
-
-    default:
-        console.log("Computer wins")
-        break;
+// let won = compare();
+// function who(won){
+//     let result = compare();
+//     if(result == "Tie"){
+//         // console.log("Inside the won");
+//         return 0;
+//     }
+//     else if(result == "Computer"){
+//         return 1;
+//     }
+//     else{
+//         return 2;
+//     }
+// }
+function game(){
+    let playerCount = 0;
+    let computerCount = 0;
+    
+    for(i=0;i<5;i++){
+        const playerChoice = prompt("Choose rock papers or scissors","rock");
+        const computerSelection = getComputerChoice();
+        let playerSelection = playerChoice.toLowerCase();
+        let won = compare(playerSelection,computerSelection);
+        if(won== "Tie"){
+            console.log(`Player chose" ${playerSelection}  "and computer chose "  ${computerSelection} "so its a tie` );
+            continue;
+        }else if(won == "Player"){
+            // console.log("Player won");
+            console.log(`Player chose" ${playerSelection}  " and computer chose " ${computerSelection} " so player won` );
+            playerCount++;
+        }
+        else{
+            console.log(`Player chose"  ${playerSelection} " and computer chose "  ${computerSelection}  "so computer won `);
+            computerCount++;
+        }
+    }
+    if(playerCount > computerCount){
+        console.log("Player won");
+    }else if(playerCount < computerCount){
+        console.log("Computer Wins");
+    }else(
+        console.log("It's is tie")
+    )
 }
+game()
