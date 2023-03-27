9356154ccac5bc8512123f240ad511149bfd5d71
Saddist <74913287+Saddist@users.noreply.github.com>
2023-03-28 00:44:49 +0530

    Blueprint is done

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Rock-paper-scissors
+this this is a raw java script for rock-papers-scissors and designing is required and I'll do it sooner with frameworks.
---
+++ index.js
@@ -0,0 +1,31 @@
+const options = ["rock", "paper", "scissors"];
+function getComputerChoice() {
+    const choice = options[Math.floor(Math.random() * options.length)];
+    return choice;
+}
+let computerSelection = getComputerChoice();
+// console.log(computerSelection);
+let playerSelection = prompt("Choose rock papers or scissors","rock");
+function compare(playerSelection, computerSelection){
+    if(playerSelection == computerSelection){
+        return "Tie";
+    }
+    else if(playerSelection == "rock" && computerSelection == "scissors" || playerSelection == "scissors" && computerSelection == "paper" ||
+    playerSelection == "paper" && computerSelection == "scissors"){
+        return "Player";
+    }
+    else {
+        return "Computer";
+    }
+}
+switch (compare(playerSelection, computerSelection)) {
+    case "Tie":
+        console.log("NO one wins");
+        break;
+    case "Player":
+        console.log("Player wins");
+
+    default:
+        console.log("Computer wins")
+        break;
+}
